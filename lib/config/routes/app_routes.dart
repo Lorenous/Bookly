ddbb0f9ce8c9d270a7de3af0ff8bc941cb5d3a24
Lorenous <osamaabaelziz@gmail.com>
2025-03-23 08:09:19 +0200

    splash view done

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,13 +1,19 @@
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRoutes {
+  static const String homeView = '/homeView';
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
-      )
+      ),
+      GoRoute(
+        path: homeView,
+        builder: (context, state) => const HomeView(),
+      ),
     ],
   );
 }
--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,8 +1,25 @@
+import 'package:bookly/config/routes/app_routes.dart';
 import 'package:bookly/core/constants/app_images.dart';
+import 'package:bookly/core/constants/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => GoRouter.of(context).go(AppRoutes.homeView),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +28,9 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AppImages.logo),
-        const Text(
+        Text(
           'Read Your Books',
+          style: Styles.textStyle18.copyWith(fontWeight: FontWeight.bold),
           textAlign: TextAlign.center,
         ),
       ],
