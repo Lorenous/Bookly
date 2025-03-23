956e6c3d1e684b6fd5d14fff1ae0717883dbf64f
Lorenous <osamaabaelziz@gmail.com>
2025-03-23 08:26:17 +0200

    create homeViewAppBar

---
+++ lib/features/home/presentation/widgets/home_view_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:bookly/core/constants/app_images.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewAppBar extends StatelessWidget {
+  const HomeViewAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AppImages.logo,
+            width: 75,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
