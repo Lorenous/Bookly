472c0f65acaade97331acdd1c59d89f16fd9f0f0
Lorenous <osamaabaelziz@gmail.com>
2025-03-28 09:41:15 +0200

    created home use cases

---
+++ lib/features/home/domain/use_cases/fetch_home_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchHomeBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo _homeRepo;
+
+  FetchHomeBooksUseCase({required HomeRepo homeRepo}) : _homeRepo = homeRepo;
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async =>
+      await _homeRepo.fetchHomeBooks();
+}
---
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/core/error/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, void> {
+  final HomeRepo _homeRepo;
+
+  FetchNewestBooksUseCase({required HomeRepo homeRepo}) : _homeRepo = homeRepo;
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async =>
+      await _homeRepo.fetchNewestBooks();
+}
