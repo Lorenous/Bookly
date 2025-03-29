47c56348de069602155919e230f8e3d1ed99f861
Lorenous <osamaabaelziz@gmail.com>
2025-03-29 07:07:03 +0200

    add hive to project

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
-class BookEntity {
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class BookEntity extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String authorName;
+  @HiveField(2)
   final String image;
+  @HiveField(3)
   final num price;
+  @HiveField(4)
   final num ratingAvg;
+  @HiveField(5)
   final num ratingCount;
 
   BookEntity({
