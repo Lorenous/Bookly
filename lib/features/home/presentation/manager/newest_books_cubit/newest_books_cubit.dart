79712e91c90fbed2bd044ff3fde3ce21d851ac8c
Lorenous <osamaabaelziz@gmail.com>
2025-03-29 09:26:19 +0200

    create newest books cubit

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit({required FetchNewestBooksUseCase fetchNewestBooksUseCase})
+      : _fetchNewestBooksUseCase = fetchNewestBooksUseCase,
+        super(NewestBooksInitial());
+  final FetchNewestBooksUseCase _fetchNewestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    final result = await _fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) => emit(
+        NewestBooksFailure(errMessage: failure.errMessage),
+      ),
+      (books) => emit(
+        NewestBooksSuccess(books: books),
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,19 @@
+part of 'newest_books_cubit.dart';
+
+sealed class NewestBooksState {}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  NewestBooksSuccess({required this.books});
+}
+
+final class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFailure({required this.errMessage});
+}
