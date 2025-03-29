52cf2e9af672253055a5699a84aaf634354e0bb8
Lorenous <osamaabaelziz@gmail.com>
2025-03-29 08:26:49 +0200

    handle server failure and home repo impl done

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,1 +1,61 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  const Failure({required this.errMessage});
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.errMessage});
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return const ServerFailure(
+            errMessage:
+                'Connection timeout, please check your internet and try again!');
+      case DioExceptionType.sendTimeout:
+        return const ServerFailure(
+            errMessage: 'Request took too long to send, please try again!');
+      case DioExceptionType.receiveTimeout:
+        return const ServerFailure(
+            errMessage:
+                'Server response took too long, please try again later!');
+      case DioExceptionType.badCertificate:
+        return const ServerFailure(
+            errMessage:
+                'Invalid security certificate from server, connection not secure!');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromBadResponse(
+          dioException.response!.statusCode!,
+          dioException.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return const ServerFailure(
+            errMessage: 'Request was canceled, please try again!');
+      case DioExceptionType.connectionError:
+        return const ServerFailure(
+            errMessage:
+                'Failed to connect to server, please check your internet!');
+      case DioExceptionType.unknown:
+        return const ServerFailure(
+            errMessage:
+                'An unexpected error occurred, please try again later!');
+    }
+  }
+
+  factory ServerFailure.fromBadResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errMessage: response['error']['message']);
+    } else if (statusCode == 404) {
+      return const ServerFailure(errMessage: 'Request Not Found');
+    } else if (statusCode == 500) {
+      return const ServerFailure(
+          errMessage: 'Internal Server Error, please try again later!');
+    } else {
+      return const ServerFailure(
+          errMessage: 'Oops there was an error, please try again later!');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/features/home/data/data_sources/home_remote_data_source.d
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final HomeRemoteDataSource _homeRemoteDataSource;
@@ -22,20 +23,52 @@ class HomeRepoImpl implements HomeRepo {
       if (books.isNotEmpty) return right(books);
       books = await _homeRemoteDataSource.fetchHomeBooks();
       return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(e));
     } catch (e) {
-      return left(Failure());
+      return left(
+        ServerFailure(
+          errMessage: e.toString(),
+        ),
+      );
     }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = _homeLocalDataSource.getNewestBooks();
+      if (books.isNotEmpty) return right(books);
+      books = await _homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(e));
+    } catch (e) {
+      return left(
+        ServerFailure(
+          errMessage: e.toString(),
+        ),
+      );
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchRelatedBooks() {
-    // TODO: implement fetchRelatedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchRelatedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = _homeLocalDataSource.getRelatedBooks();
+      if (books.isNotEmpty) return right(books);
+      books = await _homeRemoteDataSource.fetchRelatedBooks();
+      return right(books);
+    } on DioException catch (e) {
+      return left(ServerFailure.fromDioException(e));
+    } catch (e) {
+      return left(
+        ServerFailure(
+          errMessage: e.toString(),
+        ),
+      );
+    }
   }
 }
